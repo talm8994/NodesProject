d6ac00ac7917c0ea6b851ff4c612d68dc2f098b9
talm8994 <tom.tom.pop13@gmail.com>
2016-03-18 13:05:36 -0600

    nodededededed]

--- Model/CTECList.h
+++ Model/CTECList.h
@@ -7,18 +7,31 @@
 
 #ifndef MODEL_CTECLIST_H_
 #define MODEL_CTECLIST_H_
+# include "ArrayNode.h"
 
 template<class Type>
 class CTECList
 {
 private:
 	int size;
-	ArrayNode<Type> * head;
-	ArrayNode<Type> * end;
+    ArrayNode<Type> * head;
+    ArrayNode<Type> * end;
+    
+	
 public:
-	getSize(): int
-	CTECList();
-	virtual ~CTECList();
+    CTECList();
+    virtual ~CTECList();
+    int getSize();
+    void addToFront(const Type& value);
+    void addToEnd(const Type& value);
+    void addAtIndex(int index, const Type& value);
+    Type getFront();
+    Type getEnd();
+    Type getFromIndex(int index);
+    Type removeFromFront();
+    Type removeFromEnd();
+    Type removeFromIndex(int index);
+    Type set(int index, const Type& value);
     int indexOf(Type searchValue);
 };
 
