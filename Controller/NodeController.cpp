b867c288412122cd324aae084b1610b3d28a9ff4
talm8994 <tom.tom.pop13@gmail.com>
2016-03-31 12:02:28 -0600

    new

--- Controller/NodeController.cpp
+++ Controller/NodeController.cpp
@@ -57,3 +57,42 @@ void NodeController::sortData()
     
 }
 
+void NodeController::doMergesort()
+{
+    mergeData = new int[500000];
+    
+    for(int spot = 0; spot < 500000; spot++)
+    {
+        int myRandom = rand();
+        mergeData[spot] = myRandom;
+    }
+    for (int spor =0; spot < 5000; spot++)
+    {
+        count << mergeData[spot] << ", "
+    }
+    
+    Timer mergeTimer;
+    mergeTimer.startTimer();
+    mergesort(mergeData, 5000000);
+    mergeTimer.stopTimer();
+    mergeTimer.displayInformation();
+    
+    
+    delete [] mergeData;
+}
+
+void NodeController::mergesort(int data[], int size)
+{
+    int sizeOne;
+    int siztTwo;
+    
+    if(size > 1)
+    {
+        sizeOne = size/2;
+        sizeTwo = size-sizeOne;
+
+        mergesort(data, sizeOne);
+        mergesort(data+sizeOne), sizeTwo)
+    }
+}
+
