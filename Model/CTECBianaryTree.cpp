cee413650a00213c319cfe7c1e9778c4f4aec717
talm8994 <tom.tom.pop13@gmail.com>
2016-04-11 13:18:38 -0600

    Sith sais a accoudent

---
+++ Model/CTECBianaryTree.cpp
@@ -0,0 +1,9 @@
+//
+//  CTECBianaryTree.cpp
+//  NodesProject
+//
+//  Created by Almeida, Thomas on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "CTECBianaryTree.hpp"
---
+++ Model/CTECBianaryTree.hpp
@@ -0,0 +1,35 @@
+//
+//  CTECBianaryTree.hpp
+//  NodesProject
+//
+//  Created by Almeida, Thomas on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef CTECBianaryTree_hpp
+#define CTECBianaryTree_hpp
+
+#include "treeNode.cpp"
+#endif /* CTECBianaryTree_hpp */
+
+namespace CTECData
+{
+    class CTECBianaryTree_hpp
+    {
+    private:
+        int size;
+        TreeNode<Type> * root;
+        int hight;
+        bool balanced;
+    public:
+        CTECBianaryTree_hpp();
+        ~CTECBianaryree();
+        bool inster( const Type& value);
+        type remove (cont Type& value);
+        bool contains(Type value);
+        int getSize();
+        int getHight();
+        bool isBalanced
+        
+    };
+}
