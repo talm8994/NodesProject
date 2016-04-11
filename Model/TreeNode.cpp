492f076dcd317ff9723e19564a18ebbbb5c4d1b9
talm8994 <tom.tom.pop13@gmail.com>
2016-04-11 12:10:44 -0600

    ; gg;od 78eht E

---
+++ Model/TreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  TreeNode.cpp
+//  NodesProject
+//
+//  Created by Almeida, Thomas on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "TreeNode.hpp"
---
+++ Model/TreeNode.hpp
@@ -0,0 +1,34 @@
+//
+//  TreeNode.hpp
+//  NodesProject
+//
+//  Created by Almeida, Thomas on 4/11/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef TreeNode_hpp
+#define TreeNode_hpp
+
+#include "node.h"
+
+namespace CTECData
+{
+    template <class Type>
+    class TreeNode_hpp : public Node<Type>
+    {
+    private:
+        TreeNode_hpp<Type> * leftChild;
+        TreeNode_hpp<Type> * rightChild;
+        TreeNode_hpp<Type> * parent;
+    public:
+        TreeNode_hpp();
+        TreeNode_hpp(sonst Type& value);
+        TreeNode_hpp(const Type& value, TreeNode_hpp<Type> * parent)
+        void setLeftChild(TreeNode_hpp<Type> * leftChild);
+        void setRightChild(TreeNode_hpp<Type> * rightChild);
+        void setparent(TreeNode_hpp<Type> * parent);
+        TreeNode_hpp<Type> * getLeftChild();
+    };
+}
+
+#endif /* TreeNode_hpp */
